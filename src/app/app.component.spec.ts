57905d80799677a0f30c96d8febebf0b2dbc44a7
Tuschai Onglek <jasonbeer@Tuschais-MacBook-Pro.local>
2019-01-23 03:38:19 +0700

    Add CSS

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -43,16 +43,4 @@ describe('AppComponent', () => {
     const search = searchComp.debugElement.nativeElement;
     expect(search.querySelector('div.result')).toBeTruthy();
   });
-
-  // it(`should have as title 'exercise'`, async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('exercise');
-  // }));
-  // it('should render title in a h1 tag', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to exercise!');
-  // }));
 });
